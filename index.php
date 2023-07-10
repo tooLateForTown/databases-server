190722dd41ce58a9a060bea2e252bb7297a62c4e
Derek James <derek2344@protonmail.com>
2023-07-10 14:41:51 -0400

    connect to mysql

--- index.php
+++ index.php
@@ -8,5 +8,21 @@
 <body>
 <h1>Group Project Test</h1>
 <h3><?php echo 'hello world'; ?></h3>
+
+<?php
+echo "Attempting to connect to MySql...";
+$servername = "ddc353_1";
+$username = "ddc353_1";
+$password = "12pass34";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
 </body>
 </html>
