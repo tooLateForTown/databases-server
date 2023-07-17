540ec16c5d8b2490337febf8ae5a8d4456e5fd4f
Derek James <derek2344@protonmail.com>
2023-07-17 13:21:45 -0400

    Added rosemont

--- warmup/DML.sql
+++ warmup/DML.sql
@@ -3,4 +3,7 @@ INSERT INTO Ministries (name) VALUES ('Ministry of Education');  #ministryID=1
 INSERT INTO Facilities (ministryID, name, address, city, province, postalCode, phoneNumber, web, capacity, isManagement)
 VALUES (1,'Management Montréal Branch','1200 Blvd. Saint-Laurent','Montréal','QC','H197B2','514-222-2222','www.montreal.educanada.ca',2000,true);
 INSERT INTO Facilities (ministryID, name, address, city, province, postalCode, phoneNumber, web, capacity, isManagement)
-VALUES (1,'Education Montréal Branch','1200 Blvd. Saint-Laurent','Montréal','QC','H197B2','514-222-2222','www.montreal.educanada.ca',2000,true);
+VALUES (1,'Education Montréal Branch','1200 Blvd. Saint-Laurent','Montréal','QC','H197B2','514-222-2222','www.montreal.educanada.ca',2000,false);
+INSERT INTO Facilities (ministryID, name, address, city, province, postalCode, phoneNumber, web, capacity, isManagement)
+VALUES (1,'Rosemont Elementary School','100 Some Stree','Rosemont','QC','P197B2','514-222-4444','rosemont.montreal.educanada.ca',1000,false);
+
