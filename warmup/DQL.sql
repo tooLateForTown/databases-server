88c7d77ac94e81a15b79db9cd5f09b0642037e91
Derek James <derek2344@protonmail.com>
2023-07-19 11:12:29 -0400

    Updated 3-1

--- warmup/DQL.sql
+++ warmup/DQL.sql
@@ -1,36 +1,17 @@
 # question 3i (Not working)
 SELECT HeadOffices.name, HeadOffices.ministryID as HeadOfficeMinistryID, HeadOffices.Province, HeadOffices.firstName, HeadOffices.lastName,
-       COUNT(Facilities.ministryID) as numFacilities, COUNT(Employee.personID) as Employees
-FROM Ministries, Facilities, Employee, Student,
+       COUNT(Facilities.ministryID) as numFacilities, COUNT(Employees.personID) as Employees
+FROM Ministries, Facilities, Employees, Students,
         (SELECT Ministries.ministryID, Ministries.name, Facilities.province as Province, Persons.firstName, Persons.lastName
-        FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
+        FROM Facilities, Ministries, Persons, Employees, EmploymentRoles
         WHERE Facilities.ministryID = Ministries.ministryID
-          AND Employee.facilityID = Facilities.facilityID
-          AND Persons.personID = Employee.personID
-          AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
+          AND Employees.facilityID = Facilities.facilityID
+          AND Persons.personID = Employees.personID
+          AND Employees.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
           AND Facilities.isManagementHeadOffice=true
           AND EmploymentRoles.isHead = true) as HeadOffices
 WHERE Facilities.ministryID = HeadOffices.ministryID
     AND Ministries.ministryID = HeadOffices.ministryID
-    AND Employee.facilityID = Facilities.facilityID
-    AND Employee.endDate IS NULL
+    AND Employees.facilityID = Facilities.facilityID
+    AND Employees.endDate IS NULL
 GROUP BY Facilities.ministryID;
-
-SELECT Ministries.name, Facilities.province, Persons.firstName, Persons.lastName
-FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
-WHERE Facilities.ministryID = Ministries.ministryID
-  AND Employee.facilityID = Facilities.facilityID
-  AND Persons.personID = Employee.personID
-  AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
-  AND Facilities.isManagementHeadOffice=true
-  AND EmploymentRoles.isHead = true;
-
-
-SELECT Ministries.ministryID, Ministries.name, Facilities.province, Persons.firstName, Persons.lastName
-        FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
-        WHERE Facilities.ministryID = Ministries.ministryID
-          AND Employee.facilityID = Facilities.facilityID
-          AND Persons.personID = Employee.personID
-          AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
-          AND Facilities.isManagementHeadOffice=true
-          AND EmploymentRoles.isHead = true
