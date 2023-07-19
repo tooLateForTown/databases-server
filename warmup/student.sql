f7d70e31af50e4ed5101e9ac6f72b1aa26ef1302
Marwan <marwanghaziri.m@gmail.com>
2023-07-19 11:51:15 -0400

    started students file

---
+++ warmup/student.sql
@@ -0,0 +1,389 @@
+#Studentss
+#Managment Montreal Branch
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (1,3, '2005-01-01','2017-01-01','Secondary 3')
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (2, 4, '2006-02-01', '2018-02-01', 'Secondary 3');
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (3, 2, '2007-03-01', '2019-03-01', 'Secondary 3');
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (4, 1, '2008-04-01', '2020-04-01', 'Secondary 3');
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (5, 5, '2009-05-01', '2021-05-01', 'Secondary 3');
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+    VALUES (6, 3, '2010-06-01', '2022-06-01', 'Secondary 3');
+#Management Head Office
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (1, 1, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (2, 1, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (3, 1, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (4, 2, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (5, 2, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (6, 2, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (7, 3, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (8, 3, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (9, 3, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (10, 3, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#Laval Secondary School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (11, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (12, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (13, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (14, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (15, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (16, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (17, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (18, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (19, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (20, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#Rosemont Elementary School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (11, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (12, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (13, 4, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (14, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (15, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (16, 5, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (17, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (18, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (19, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (20, 6, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#Westmount High School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (21, 7, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (22, 7, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (23, 7, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (24, 8, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (25, 8, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (26, 8, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (27, 9, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (28, 9, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (29, 9, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (30, 9, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#John Rennie High School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (31, 10, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (32, 10, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (33, 10, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (34, 11, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (35, 11, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (36, 11, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (37, 12, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (38, 12, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (39, 12, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (40, 12, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#Evangeline
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (41, 13, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (42, 13, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (43, 14, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (44, 14, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (45, 15, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (46, 15, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (47, 16, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (48, 16, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (49, 17, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (50, 17, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+#Evergreen Elementary
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (51, 18, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (52, 18, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (53, 19, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (54, 19, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (55, 20, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (56, 20, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (57, 21, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (58, 21, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (59, 22, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (60, 22, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+#Hogwarts Elementary
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (61, 23, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (62, 23, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (63, 24, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (64, 24, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (65, 25, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (66, 25, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (67, 26, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (68, 26, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (69, 27, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (70, 27, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#St. Thomas High School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (71, 28, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (72, 28, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (73, 29, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (74, 29, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (75, 30, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (76, 30, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (77, 31, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (78, 31, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (79, 32, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (80, 32, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+#Québec City Middle School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (81, 33, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (82, 33, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (83, 34, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (84, 34, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (85, 35, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (86, 35, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (87, 36, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (88, 36, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (89, 37, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (90, 37, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+#Sherbrooke High School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (91, 38, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (92, 38, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (93, 39, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (94, 39, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (95, 40, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (96, 40, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (97, 41, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (98, 41, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (99, 42, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (100, 42, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+#Trois-Rivières Elementary School
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (101, 43, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (102, 43, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (103, 44, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (104, 44, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (105, 45, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (106, 45, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (107, 46, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (108, 46, '2023-01-01', '2023-12-31', 'Secondary 4');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (109, 47, '2023-01-01', '2023-12-31', 'Secondary 2');
+
+INSERT INTO Students (personID, facilityID, startDate, endDate, grade)
+VALUES (110, 47, '2023-01-01', '2023-12-31', 'Secondary 3');
+
+#Gatineau Secondary School
+
+
