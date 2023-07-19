192da6d0c89465303636b2fa515ac10f58bf12fe
Derek James <derek2344@protonmail.com>
2023-07-19 11:05:07 -0400

    Worked on query 1

--- warmup/DQL.sql
+++ warmup/DQL.sql
@@ -1,9 +1,8 @@
 # question 3i (Not working)
-SELECT HeadOffices.name,
-       COUNT(Facilities.facilityID) as numFacilities
-
-FROM Ministries, Facilities,
-        (SELECT Ministries.name, Facilities.province, Persons.firstName, Persons.lastName
+SELECT HeadOffices.name, HeadOffices.ministryID as HeadOfficeMinistryID, HeadOffices.Province, HeadOffices.firstName, HeadOffices.lastName,
+       COUNT(Facilities.ministryID) as numFacilities, COUNT(Employee.personID) as Employees
+FROM Ministries, Facilities, Employee, Student,
+        (SELECT Ministries.ministryID, Ministries.name, Facilities.province as Province, Persons.firstName, Persons.lastName
         FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
         WHERE Facilities.ministryID = Ministries.ministryID
           AND Employee.facilityID = Facilities.facilityID
@@ -11,13 +10,11 @@ FROM Ministries, Facilities,
           AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
           AND Facilities.isManagementHeadOffice=true
           AND EmploymentRoles.isHead = true) as HeadOffices
-
-GROUP BY Facilities.province;
-
-
-
-
-
+WHERE Facilities.ministryID = HeadOffices.ministryID
+    AND Ministries.ministryID = HeadOffices.ministryID
+    AND Employee.facilityID = Facilities.facilityID
+    AND Employee.endDate IS NULL
+GROUP BY Facilities.ministryID;
 
 SELECT Ministries.name, Facilities.province, Persons.firstName, Persons.lastName
 FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
@@ -27,3 +24,13 @@ WHERE Facilities.ministryID = Ministries.ministryID
   AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
   AND Facilities.isManagementHeadOffice=true
   AND EmploymentRoles.isHead = true;
+
+
+SELECT Ministries.ministryID, Ministries.name, Facilities.province, Persons.firstName, Persons.lastName
+        FROM Facilities, Ministries, Persons, Employee, EmploymentRoles
+        WHERE Facilities.ministryID = Ministries.ministryID
+          AND Employee.facilityID = Facilities.facilityID
+          AND Persons.personID = Employee.personID
+          AND Employee.primaryEmploymentRoleID = EmploymentRoles.employmentRoleID
+          AND Facilities.isManagementHeadOffice=true
+          AND EmploymentRoles.isHead = true
