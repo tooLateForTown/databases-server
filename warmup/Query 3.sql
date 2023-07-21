7cf8e3a6065ef1135a0d526081b410b1a8fefb0a
Marwan <marwanghaziri.m@gmail.com>
2023-07-21 18:06:22 -0400

    Query 3 update

--- warmup/Query 3.sql
+++ warmup/Query 3.sql
@@ -1,25 +1,34 @@
 SELECT
-    Persons.firstName,
-    Persons.lastName,
-    Persons.dateOfBirth,
-    Persons.medicare,
-    Persons.medicareExpiryDate,
-    Persons.phoneNumber,
-    Persons.email
+    P.firstName,
+    P.lastName,
+    P.dateOfBirth,
+    P.medicare,
+    P.medicareExpiryDate,
+    P.phoneNumber,
+    P.email
 FROM
-    Students
-JOIN
-    Persons ON Students.personID = Persons.personID
+    Persons P
+JOIN (
+    SELECT
+        S.personID,
+        S.facilityID,
+        MAX(S.startDate) AS latestStartDate
+    FROM
+        Students S
+    WHERE
+        S.facilityID IN (SELECT facilityID FROM Facilities WHERE name = 'Rosemont Elementary School')
+    GROUP BY
+        S.personID, S.facilityID
+) AS LatestStudents ON P.personID = LatestStudents.personID
+LEFT JOIN
+    Students S ON LatestStudents.personID = S.personID AND LatestStudents.facilityID = S.facilityID AND LatestStudents.latestStartDate = S.startDate
+LEFT JOIN
+    Infections I ON P.personID = I.personID
+LEFT JOIN
+    InfectionTypes IT ON I.infectionTypeID = IT.name
 WHERE
-    Students.facilityID = (SELECT facilityID FROM Facilities WHERE name = 'Rosemont Elementary School')
-    AND Students.personID IN (
-        SELECT
-            personID
-        FROM
-            Infections
-        WHERE
-            type = 'COVID-19'
-    )
-    AND Persons.medicareExpiryDate < CURDATE()
+    IT.name = 'COVID-19'
+    AND P.medicareExpiryDate < CURDATE()
 ORDER BY
-    Persons.medicareExpiryDate ASC;
+    P.medicareExpiryDate ASC;
+
