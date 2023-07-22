494db5167362c34ffd739197a18b105d86148e8e
IcedCaffeine <fahad7923@gmail.com>
2023-07-22 17:13:27 -0400

    Completed query for part 3 question 8

--- warmup/DQL3-8.sql
+++ warmup/DQL3-8.sql
@@ -1,5 +1,5 @@
 # Question 3-8
-SELECT InfectionTypes.name, Ministries.name, COUNT(Students.personID) AS TotalInfections
+SELECT InfectionTypes.name, Ministries.name, COUNT(DISTINCT Students.personID) AS TotalInfections
 FROM InfectionTypes, Ministries, Infections, Students, Facilities
 WHERE InfectionTypes.infectionTypeID = Infections.infectionTypeID
     AND Infections.personID = Students.personID
@@ -7,5 +7,3 @@ WHERE InfectionTypes.infectionTypeID = Infections.infectionTypeID
     AND Facilities.ministryID = Ministries.ministryID
 GROUP BY InfectionTypes.name, Ministries.name
 ORDER BY InfectionTypes.name ASC, TotalInfections DESC;
-
-#fixme:  Should be number of students, not infections.  Some students are infected more than once.
