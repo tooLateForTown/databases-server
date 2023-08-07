72e63ad05745e9f6b758a323235f4d3db0e5eee7
samdarshihawk <punisher.sammy@gmail.com>
2023-08-07 19:27:04 -0400

    Created a email generation page that takes user to email_log.php when the button is clicked

--- main/web/ministries.php
+++ main/web/ministries.php
@@ -11,7 +11,7 @@
 <h1>Ministries</h1>
 <?php
     $sql = "SELECT Ministries.ministryID, Ministries.name, count(Facilities.ministryID) as count FROM Ministries LEFT JOIN Facilities ON Facilities.ministryID = Ministries.ministryID GROUP BY Ministries.ministryID";
-    generateMasterTable($sql,"edit_ministry.php");
+    generateMasterTable($sql,"edit_ministry.php", 0, 1,-1,"Ministry ID", "Name","", isStudent:false, isEmployee:false);
 ?>
 </main>
 </body>
