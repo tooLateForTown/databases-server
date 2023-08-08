a88aaacb5270976f4fc444af4351e5b133b85828
Derek James <derek2344@protonmail.com>
2023-08-08 17:14:19 -0400

    added team member names

--- main/web/index.php
+++ main/web/index.php
@@ -9,11 +9,11 @@
 <main>
 <h1>COMP 353 Main Project</h1>
 <h3>Summer 2023</h3>
-<h2>Team</h2>
-<p>member 1</p>
-<p>member 1</p>
-<p>member 1</p>
-<p>member 1</p>
+<h2>Team ddc353_1</h2>
+<p>Derek James, 22688964</p>
+<p>Fahad Abdul Rahman 40157997</p>
+<p>Marwan Al-Ghaziri 40126554</p>
+<p>Samdarshi Tiwari 40113256</p>
 
 
 </main>
