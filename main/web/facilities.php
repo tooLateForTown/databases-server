f0611303c7809e10ad432463a9cc623a8837c8cb
Derek James <derek2344@protonmail.com>
2023-08-08 13:06:20 -0400

    fixed facilities list

--- main/web/facilities.php
+++ main/web/facilities.php
@@ -10,7 +10,12 @@
 <main>
 <h1>Facilities</h1>
 <?php
-    generateMasterTable("SELECT Facilities.facilityID, Facilities.name, count(Employees.personID)+count(Students.personID) as count FROM Facilities, Students, Employees WHERE Students.facilityID=Facilities.facilityID AND Employees.facilityID=Facilities.facilityID AND Students.endDate IS NULL and Employees.endDate IS NULL GROUP BY Facilities.facilityID","edit_facility.php", 0,1,2,"ID","Name","Record",isStudent:false, isEmployee:false);
+    $sql="SELECT Facilities.facilityID, Facilities.name, count(Employees.personID)+count(Students.personID) as count FROM Facilities
+    LEFT OUTER JOIN  Students ON Facilities.facilityID=Students.facilityID
+    LEFT OUTER JOIN Employees ON Facilities.facilityID = Employees.facilityID
+    WHERE Students.endDate IS NULL and Employees.endDate IS NULL 
+    GROUP BY Facilities.facilityID";
+    generateMasterTable($sql,"edit_facility.php", 0,1,2,"ID","Name","Record",isStudent:false, isEmployee:false);
 ?>
 </main>
 </body>
